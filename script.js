7ee60f8cee2242a59821e2d65ec88bda5e0d294a
Bernard Telles <bernardtelgon@gmail.com>
2020-11-02 01:09:06 -0300

    builting calculator 3 day

---
+++ script.js
@@ -0,0 +1,11 @@
+function getHistoty(){
+  return document.getElementById("history-value").innerText;
+
+}
+function printHistory(num){
+  document.getElementById("history-value").innerText=num;
+}
+
+function printOutput(num){
+  document.getElementById("output-val").innerText=num;
+}
