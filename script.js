b728d255d9a3c02adee1abbd09b0a438f12d7966
Bernard Telles <bernardtelgon@gmail.com>
2020-11-03 00:15:52 -0300

    builting calculator day4

--- script.js
+++ script.js
@@ -7,5 +7,27 @@ function printHistory(num){
 }
 
 function printOutput(num){
-  document.getElementById("output-val").innerText=num;
+  return document.getElementById("output-value").innerText;
 }
+
+function printOutput(num){
+  if(num==""){
+    document.getElementById("output-value").innerText=num
+  }
+  else{
+    document.getElementById("output-value").innerText=getFormattedNumber(num);
+  }
+  
+}
+
+function getFormattedNumber(num){
+  var n = Number(num);
+  var value = n.toLocaleString("en");
+  return value;
+}
+
+printOutput("98765433567");
+function reverNumberFormat(num){
+  return Number(num.replace(/,/g,''));
+}
+alert(reverNumberFormat(getOutput()));
